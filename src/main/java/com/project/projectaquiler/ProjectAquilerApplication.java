5b807bd2942afd9a6ec35c1d1e80e314b119f81e
bryanby2002 <brayan7br7@gmail.com>
2024-04-27 17:03:56 -0500

    commit initial

---
+++ src/main/java/com/project/projectaquiler/ProjectAquilerApplication.java
@@ -0,0 +1,13 @@
+package com.project.projectaquiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAquilerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAquilerApplication.class, args);
+    }
+
+}
