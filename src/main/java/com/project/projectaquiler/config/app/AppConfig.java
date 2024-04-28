ab786dc05f3e4e2d81e8a1a6093fee0a4df60e5b
bryanby2002 <brayan7br7@gmail.com>
2024-04-28 10:58:23 -0500

    configure variables environment

--- src/main/java/com/project/projectaquiler/config/app/AppConfig.java
+++ src/main/java/com/project/projectaquiler/config/app/AppConfig.java
@@ -5,20 +5,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class AppConfig {
 
+    private Map<String, String> loadEnviromentVariables(){
+        Map<String, String> enVars = new HashMap<>();
+        enVars.put("DB_HOST", System.getenv("DB_HOST"));
+        enVars.put("DB_PORT", System.getenv("DB_PORT"));
+        enVars.put("DB_NAME", System.getenv("DB_NAME"));
+        enVars.put("DB_USER", System.getenv("DB_USER"));
+        enVars.put("DB_PASSWORD", System.getenv("DB_PASSWORD"));
+        return enVars;
+    }
+
     @Bean
     public DataSource getDataSource() {
-
+        Map<String, String> enVars = loadEnviromentVariables();
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/alquiler_db");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
-
+        dataSource.setUrl("jdbc:mysql://"+enVars.get("DB_HOST")+":"+enVars.get("DB_PORT")+"/"+enVars.get("DB_NAME"));
+        dataSource.setUsername(enVars.get("DB_USER"));
+        dataSource.setPassword(enVars.get("DB_PASSWORD"));
         return dataSource;
     }
 }
