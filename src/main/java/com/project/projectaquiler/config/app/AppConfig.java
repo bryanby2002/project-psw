c45f1b4682bb5b9eaf5b4f597dab4f242741affb
bryanby2002 <brayan7br7@gmail.com>
2024-05-21 19:47:13 -0500

    format code prettier

--- src/main/java/com/project/projectaquiler/config/app/AppConfig.java
+++ src/main/java/com/project/projectaquiler/config/app/AppConfig.java
@@ -1,37 +1,38 @@
 package com.project.projectaquiler.config.app;
 
+import java.util.HashMap;
+import java.util.Map;
+import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
-
 @Configuration
 @Slf4j
 public class AppConfig {
 
-    private Map<String, String> loadEnviromentVariables(){
-        Map<String, String> enVars = new HashMap<>();
-        enVars.put("DB_HOST", System.getenv("DB_HOST"));
-        enVars.put("DB_PORT", System.getenv("DB_PORT"));
-        enVars.put("DB_NAME", System.getenv("DB_NAME"));
-        enVars.put("DB_USER", System.getenv("DB_USER"));
-        enVars.put("DB_PASSWORD", System.getenv("DB_PASSWORD"));
-        return enVars;
-    }
+  private Map<String, String> loadEnviromentVariables() {
+    Map<String, String> enVars = new HashMap<>();
+    enVars.put("DB_HOST", System.getenv("DB_HOST"));
+    enVars.put("DB_PORT", System.getenv("DB_PORT"));
+    enVars.put("DB_NAME", System.getenv("DB_NAME"));
+    enVars.put("DB_USER", System.getenv("DB_USER"));
+    enVars.put("DB_PASSWORD", System.getenv("DB_PASSWORD"));
+    return enVars;
+  }
 
-    @Bean
-    DataSource getDataSource() {
-        Map<String, String> enVars = loadEnviromentVariables();
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://"+enVars.get("DB_HOST")+":"+enVars.get("DB_PORT")+"/"+enVars.get("DB_NAME"));
-        dataSource.setUsername(enVars.get("DB_USER"));
-        dataSource.setPassword(enVars.get("DB_PASSWORD"));
-        log.info("Database connection established");
-        return dataSource;
-    }
+  @Bean
+  DataSource getDataSource() {
+    Map<String, String> enVars = loadEnviromentVariables();
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    dataSource.setUrl(
+      "jdbc:mysql://" + enVars.get("DB_HOST") + ":" + enVars.get("DB_PORT") + "/" + enVars.get("DB_NAME")
+      );
+    dataSource.setUsername(enVars.get("DB_USER"));
+    dataSource.setPassword(enVars.get("DB_PASSWORD"));
+    log.info("Database connection established");
+    return dataSource;
+  }
 }
--- src/main/java/com/project/projectaquiler/config/security/SecurityConfig.java
+++ src/main/java/com/project/projectaquiler/config/security/SecurityConfig.java
@@ -21,57 +21,73 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(
-                        SessionCreationPolicy.STATELESS
-                ))
-                .authorizeHttpRequests(httpRequest -> {
-                    // endpoinst publicos
-                    // vehiculos
-                    httpRequest.requestMatchers(HttpMethod.GET, "/vehicle/list").permitAll();
-                    httpRequest.requestMatchers(HttpMethod.GET, "vehicle/search/{palabra}").permitAll();
-                    httpRequest.requestMatchers(HttpMethod.GET, "vehicle/filter").permitAll();
-                    httpRequest.requestMatchers(HttpMethod.POST, "/user/create").permitAll();
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http)
+    throws Exception {
+    return http
+      .csrf(AbstractHttpConfigurer::disable)
+      .httpBasic(Customizer.withDefaults())
+      .sessionManagement(session ->
+        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      )
+      .authorizeHttpRequests(httpRequest -> {
+        // endpoinst publicos
+        // vehiculos
+        httpRequest
+          .requestMatchers(HttpMethod.GET, "/vehicle/list")
+          .permitAll();
+        httpRequest
+          .requestMatchers(HttpMethod.GET, "vehicle/search/{palabra}")
+          .permitAll();
+        httpRequest
+          .requestMatchers(HttpMethod.GET, "vehicle/filter")
+          .permitAll();
+        httpRequest
+          .requestMatchers(HttpMethod.POST, "/user/create")
+          .permitAll();
 
-                    //endpoinst configurados
-                    // vehiculos
-                    httpRequest.requestMatchers(HttpMethod.POST, "/vehicle/create").hasRole("ADMIN");
-                    httpRequest.requestMatchers(HttpMethod.PATCH, "vehicle/update").hasRole("ADMIN");
+        //endpoinst configurados
+        // vehiculos
+        httpRequest
+          .requestMatchers(HttpMethod.POST, "/vehicle/create")
+          .hasRole("ADMIN");
+        httpRequest
+          .requestMatchers(HttpMethod.PATCH, "vehicle/update")
+          .hasRole("ADMIN");
 
-                    // user
-                    httpRequest.requestMatchers(HttpMethod.POST, "/user/list").hasRole("ADMIN");
-                    httpRequest.requestMatchers(HttpMethod.PATCH, "/user/bookings")
-                            .hasAnyRole("USER", "ADMIN");
+        // user
+        httpRequest
+          .requestMatchers(HttpMethod.POST, "/user/list")
+          .hasRole("ADMIN");
+        httpRequest
+          .requestMatchers(HttpMethod.PATCH, "/user/bookings")
+          .hasAnyRole("USER", "ADMIN");
 
-                    // booking
-                    httpRequest.requestMatchers(HttpMethod.POST, "/booking/create");
+        // booking
+        httpRequest.requestMatchers(HttpMethod.POST, "/booking/create");
 
-                    // endpoints no configuradas
-                    httpRequest.anyRequest().denyAll();
-                })
-                .build();
-    }
+        // endpoints no configuradas
+        httpRequest.anyRequest().denyAll();
+      })
+      .build();
+  }
 
-    @Bean
-    public AuthenticationManager manager(AuthenticationConfiguration configuration)
-            throws Exception {
-        return configuration.getAuthenticationManager();
-    }
+  @Bean
+  AuthenticationManager manager(AuthenticationConfiguration configuration)
+    throws Exception {
+    return configuration.getAuthenticationManager();
+  }
 
-    @Bean
-    public AuthenticationProvider provider(UserDetailsImpl userDetailsService){
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder());
-        provider.setUserDetailsService(userDetailsService);
-        return provider;
-    }
+  @Bean
+  AuthenticationProvider provider(UserDetailsImpl userDetailsService) {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setPasswordEncoder(passwordEncoder());
+    provider.setUserDetailsService(userDetailsService);
+    return provider;
+  }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
