4f51b074c641e743817b816218f4f3d8b8b47020
bryanby2002 <brayan7br7@gmail.com>
2024-05-28 21:21:53 -0500

    agregado record para crear roles

---
+++ src/main/java/com/project/projectaquiler/dto/request/AuhCreateRoleRequest.java
@@ -0,0 +1,11 @@
+package com.project.projectaquiler.dto.request;
+
+import jakarta.validation.constraints.Size;
+import java.util.List;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+public record AuhCreateRoleRequest(
+  @Size(max = 4, message = "El usuario no puede tener mas de 4 roles")
+  List<String> roleListName
+) {}
