4f933a8d036c155c43408535f8185b22f61f8954
bryanDev <brayan7br7@gmail.com>
2024-06-12 11:51:27 -0500

    corrigiendo errores

--- src/main/java/com/project/projectaquiler/persistence/repositories/PermissionRepository.java
+++ src/main/java/com/project/projectaquiler/persistence/repositories/PermissionRepository.java
@@ -1,7 +1,9 @@
 package com.project.projectaquiler.persistence.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PermissionRepository extends CrudRepository<PermissionRepository, String>{
     
 }
